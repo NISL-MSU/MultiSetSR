7a17b5f3bb3b10c9c30fb68500f2bbbcf71357c4
Giorgio Morales Luna <giorgiomoralesluna@gmail.com>
2024-03-04 15:55:19 -0700

    Comparison results

--- Main.py
+++ Main.py
@@ -1,7 +1,11 @@
 from src.EquationLearning.SymbolicRegressor.MSSP import SymbolicRegressor
+from src.EquationLearning.Trainer.TrainMultiSetTransformer import TransformerTrainer
 
 
 if __name__ == '__main__':
     # Dataset names: E1 - E9, CS1-CS4
-    regressor = SymbolicRegressor(dataset='E1')
-    regressor.get_skeleton()
+    # regressor = SymbolicRegressor(dataset='E1')
+    # regressor.get_skeleton()
+    trainer = TransformerTrainer()
+    trainer.fit()
+
