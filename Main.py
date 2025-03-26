a2398d5ec82b241e874a6390eb6f4cf37777d80a
Giorgio Morales Luna <giorgiomoralesluna@gmail.com>
2025-03-26 23:26:16 +0100

    README

--- Main.py
+++
@@ -1,7 +0,0 @@
-from EquationLearning.Trainer.TrainMultiSetTransformer import TransformerTrainer
-
-
-if __name__ == '__main__':
-    # Dataset names: E1 - E9, CS1-CS4
-    trainer = TransformerTrainer()
-    trainer.fit()
